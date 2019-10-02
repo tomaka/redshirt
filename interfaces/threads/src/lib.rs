1e85d45481fd757c1e6443f9fbde12fc0bc7e4bc
Pierre Krieger <pierre.krieger1708@gmail.com>
2019-10-02 15:06:32 +0200

    Move threads stuff back to threads

--- interfaces/threads/src/lib.rs
+++ interfaces/threads/src/lib.rs
@@ -9,3 +9,31 @@
 use std::mem;
 
 pub mod ffi;
+
+/// > **WARNING**: Rust (and more importantly LLVM) at the moment assumes that only a single WASM
+/// >              thread can exist at any given point in time. More specifically, LLVM assumes
+/// >              that only a single stack exists, and maintains a stack pointer as a global
+/// >              variable. It is therefore unsound to use stack variables on separate threads.
+#[cfg(target_arch = "wasm32")]
+pub unsafe fn spawn_thread(function: impl FnOnce()) {
+    let function_box: Box<Box<dyn FnOnce()>> = Box::new(Box::new(function));
+
+    extern "C" fn caller(user_data: u32) {
+        unsafe {
+            let user_data = Box::from_raw(user_data as *mut Box<dyn FnOnce()>);
+            user_data();
+        }
+    }
+
+    let thread_new = ffi::ThreadsMessage::New(ffi::ThreadNew {
+        fn_ptr: mem::transmute(caller as extern "C" fn(u32)),
+        user_data: Box::into_raw(function_box) as usize as u32,
+    });
+
+    syscalls::emit_message(&ffi::INTERFACE, &thread_new, false).unwrap();
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+pub unsafe fn spawn_thread(function: impl FnOnce()) {
+    panic!()
+}
