06891ec42a964aa5bf5f6fa65e866e558e07a167
Pierre Krieger <pierre.krieger1708@gmail.com>
2019-11-24 16:10:32 +0100

    Add support for TCP listening (#51)

--- hosted/tcp/src/lib.rs
+++ hosted/tcp/src/lib.rs
@@ -15,7 +15,11 @@
 
 //! Implements the TCP interface.
 
-use async_std::{net::TcpStream, sync::Mutex, task};
+use async_std::{
+    net::{TcpListener, TcpStream},
+    sync::Mutex,
+    task,
+};
 use fnv::FnvHashMap;
 use futures::{channel::mpsc, prelude::*};
 use std::{
@@ -44,23 +48,35 @@ pub struct TcpState {
 }
 
 /// State of a socket known from the front state.
-struct FrontSocketState {
-    /// If the socket is connected, a sender to send commands to the background task. `None` if
-    /// the socket is not connected yet.
-    sender: Option<mpsc::Sender<FrontToBack>>,
+enum FrontSocketState {
+    /// This socket ID is reserved, but not the background task is in the process of opening it.
+    Orphan,
+
+    /// The socket is connected. Contains a sender to send commands to the background task.
+    Connected(mpsc::Sender<FrontToBackSocket>),
+
+    /// The socket is a listener.
+    Listener(mpsc::Sender<FrontToBackListener>),
 }
 
 #[derive(Debug)]
 pub enum TcpResponse {
     Open(u64, nametbd_tcp_interface::ffi::TcpOpenResponse),
+    Listen(u64, nametbd_tcp_interface::ffi::TcpListenResponse),
     Read(u64, nametbd_tcp_interface::ffi::TcpReadResponse),
     Write(u64, nametbd_tcp_interface::ffi::TcpWriteResponse),
+    Accept(u64, nametbd_tcp_interface::ffi::TcpAcceptResponse),
 }
 
-/// Message sent from the main task to the background task.
-enum FrontToBack {
+/// Message sent from the main task to the background task for sockets.
+enum FrontToBackSocket {
     Read { message_id: u64 },
     Write { message_id: u64, data: Vec<u8> },
+}
+
+/// Message sent from the main task to the background task for listeners.
+enum FrontToBackListener {
+    Accept { message_id: u64 },
 }
 
 /// Message sent from a background socket task to the main task.
@@ -68,10 +84,19 @@ enum BackToFront {
     OpenOk {
         open_message_id: u64,
         socket_id: u32,
-        sender: mpsc::Sender<FrontToBack>,
+        sender: mpsc::Sender<FrontToBackSocket>,
     },
     OpenErr {
         open_message_id: u64,
+        socket_id: u32,
+    },
+    ListenOk {
+        listen_message_id: u64,
+        socket_id: u32,
+        sender: mpsc::Sender<FrontToBackListener>,
+    },
+    ListenErr {
+        listen_message_id: u64,
         socket_id: u32,
     },
     Read {
@@ -81,6 +106,10 @@ enum BackToFront {
     Write {
         message_id: u64,
         result: Result<(), ()>,
+    },
+    Accept {
+        message_id: u64,
+        socket: TcpStream,
     },
 }
 
@@ -134,7 +163,40 @@ impl TcpState {
                                 socket_addr,
                                 self.sender.clone(),
                             ));
-                            e.insert(FrontSocketState { sender: None });
+                            e.insert(FrontSocketState::Orphan);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            nametbd_tcp_interface::ffi::TcpMessage::Listen(listen) => {
+                let message_id = message_id.unwrap(); // TODO: don't unwrap; but what to do?
+                let socket_addr = {
+                    let ip_addr = Ipv6Addr::from(listen.local_ip);
+                    if let Some(ip_addr) = ip_addr.to_ipv4() {
+                        SocketAddr::new(ip_addr.into(), listen.port)
+                    } else {
+                        SocketAddr::new(ip_addr.into(), listen.port)
+                    }
+                };
+
+                // Find a vacant entry in `self.sockets`, spawn the task, and insert.
+                let mut tentative_socket_id = rand::random();
+                loop {
+                    match sockets.entry(tentative_socket_id) {
+                        Entry::Occupied(_) => {
+                            tentative_socket_id = tentative_socket_id.wrapping_add(1);
+                            continue;
+                        }
+                        Entry::Vacant(e) => {
+                            task::spawn(listener_task(
+                                tentative_socket_id,
+                                message_id,
+                                socket_addr,
+                                self.sender.clone(),
+                            ));
+                            e.insert(FrontSocketState::Orphan);
                             break;
                         }
                     }
@@ -145,15 +207,26 @@ impl TcpState {
                 let _ = sockets.remove(&close.socket_id);
             }
 
+            nametbd_tcp_interface::ffi::TcpMessage::Accept(accept) => {
+                let message_id = message_id.unwrap(); // TODO: don't unwrap; but what to do?
+                sockets
+                    .get_mut(&accept.socket_id)
+                    .unwrap() // TODO: don't unwrap; but what to do?
+                    .as_mut_listener()
+                    .unwrap()
+                    .send(FrontToBackListener::Accept { message_id })
+                    .await
+                    .unwrap(); // TODO: don't unwrap; but what to do?
+            }
+
             nametbd_tcp_interface::ffi::TcpMessage::Read(read) => {
                 let message_id = message_id.unwrap(); // TODO: don't unwrap; but what to do?
                 sockets
                     .get_mut(&read.socket_id)
                     .unwrap() // TODO: don't unwrap; but what to do?
-                    .sender
-                    .as_mut()
+                    .as_mut_connected()
                     .unwrap()
-                    .send(FrontToBack::Read { message_id })
+                    .send(FrontToBackSocket::Read { message_id })
                     .await
                     .unwrap(); // TODO: don't unwrap; but what to do?
             }
@@ -163,10 +236,9 @@ impl TcpState {
                 sockets
                     .get_mut(&write.socket_id)
                     .unwrap() // TODO: don't unwrap; but what to do?
-                    .sender
-                    .as_mut()
+                    .as_mut_connected()
                     .unwrap()
-                    .send(FrontToBack::Write {
+                    .send(FrontToBackSocket::Write {
                         message_id,
                         data: write.data,
                     })
@@ -190,9 +262,9 @@ impl TcpState {
                 sender,
             } => {
                 let mut sockets = self.sockets.lock().await;
-                let mut front_state = sockets.get_mut(&socket_id).unwrap();
-                debug_assert!(front_state.sender.is_none());
-                front_state.sender = Some(sender);
+                let front_state = sockets.get_mut(&socket_id).unwrap();
+                // TODO: debug_assert is orphan
+                *front_state = FrontSocketState::Connected(sender);
 
                 TcpResponse::Open(
                     open_message_id,
@@ -209,13 +281,78 @@ impl TcpState {
                 let mut sockets = self.sockets.lock().await;
                 let _front_state = sockets.remove(&socket_id);
                 debug_assert!(match _front_state {
-                    Some(s) if s.sender.is_none() => true,
+                    Some(FrontSocketState::Orphan) => true,
                     _ => false,
                 });
 
                 TcpResponse::Open(
                     open_message_id,
                     nametbd_tcp_interface::ffi::TcpOpenResponse { result: Err(()) },
+                )
+            }
+
+            BackToFront::ListenOk {
+                listen_message_id,
+                socket_id,
+                sender,
+            } => {
+                let mut sockets = self.sockets.lock().await;
+                let front_state = sockets.get_mut(&socket_id).unwrap();
+                // TODO: debug_assert is orphan
+                *front_state = FrontSocketState::Listener(sender);
+
+                TcpResponse::Listen(
+                    listen_message_id,
+                    nametbd_tcp_interface::ffi::TcpListenResponse {
+                        result: Ok((socket_id, 0)), // FIXME: correct port
+                    },
+                )
+            }
+
+            BackToFront::ListenErr {
+                listen_message_id,
+                socket_id,
+            } => {
+                let mut sockets = self.sockets.lock().await;
+                let _front_state = sockets.remove(&socket_id);
+                debug_assert!(match _front_state {
+                    Some(FrontSocketState::Orphan) => true,
+                    _ => false,
+                });
+
+                TcpResponse::Listen(
+                    listen_message_id,
+                    nametbd_tcp_interface::ffi::TcpListenResponse { result: Err(()) },
+                )
+            }
+
+            BackToFront::Accept { message_id, socket } => {
+                let mut sockets = self.sockets.lock().await;
+
+                // Find a vacant entry in `self.sockets`, spawn the task, and insert.
+                let mut tentative_socket_id = rand::random();
+                loop {
+                    match sockets.entry(tentative_socket_id) {
+                        Entry::Occupied(_) => {
+                            tentative_socket_id = tentative_socket_id.wrapping_add(1);
+                            continue;
+                        }
+                        Entry::Vacant(e) => {
+                            let (tx, rx) = mpsc::channel(2);
+                            task::spawn(open_socket_task(socket, rx, self.sender.clone()));
+                            e.insert(FrontSocketState::Connected(tx));
+                            break;
+                        }
+                    }
+                }
+
+                TcpResponse::Accept(
+                    message_id,
+                    nametbd_tcp_interface::ffi::TcpAcceptResponse {
+                        accepted_socket_id: tentative_socket_id,
+                        remote_ip: [0; 8], // FIXME:
+                        remote_port: 0,    // FIXME:
+                    },
                 )
             }
 
@@ -244,6 +381,22 @@ impl fmt::Debug for TcpState {
     }
 }
 
+impl FrontSocketState {
+    fn as_mut_connected(&mut self) -> Option<&mut mpsc::Sender<FrontToBackSocket>> {
+        match self {
+            FrontSocketState::Connected(sender) => Some(sender),
+            _ => None,
+        }
+    }
+
+    fn as_mut_listener(&mut self) -> Option<&mut mpsc::Sender<FrontToBackListener>> {
+        match self {
+            FrontSocketState::Listener(sender) => Some(sender),
+            _ => None,
+        }
+    }
+}
+
 /// Function executed in the background for each TCP socket.
 async fn socket_task(
     socket_id: u32,
@@ -252,7 +405,7 @@ async fn socket_task(
     mut back_to_front: mpsc::Sender<BackToFront>,
 ) {
     // First step is to try connect to the destination.
-    let (mut socket, mut commands_rx) = match TcpStream::connect(socket_addr).await {
+    let (socket, commands_rx) = match TcpStream::connect(socket_addr).await {
         Ok(s) => {
             let (tx, rx) = mpsc::channel(2);
             let msg_to_front = BackToFront::OpenOk {
@@ -277,12 +430,21 @@ async fn socket_task(
         }
     };
 
+    open_socket_task(socket, commands_rx, back_to_front).await
+}
+
+/// Function executed in the background for each TCP socket.
+async fn open_socket_task(
+    mut socket: TcpStream,
+    mut commands_rx: mpsc::Receiver<FrontToBackSocket>,
+    mut back_to_front: mpsc::Sender<BackToFront>,
+) {
     // Now that we're connected and we have a `socket` and `commands_rx`, we can start reading
     // and writing.
     loop {
         // TODO: should read and write asynchronously, but that's hard because of borrowing question
         match commands_rx.next().await {
-            Some(FrontToBack::Read { message_id }) => {
+            Some(FrontToBackSocket::Read { message_id }) => {
                 let mut read_buf = vec![0; 1024];
                 let result = socket
                     .read(&mut read_buf)
@@ -297,9 +459,61 @@ async fn socket_task(
                     return;
                 }
             }
-            Some(FrontToBack::Write { message_id, data }) => {
+            Some(FrontToBackSocket::Write { message_id, data }) => {
                 let result = socket.write_all(&data).await.map_err(|_| ());
                 let msg_to_front = BackToFront::Write { message_id, result };
+                if back_to_front.send(msg_to_front).await.is_err() {
+                    return;
+                }
+            }
+            None => {
+                // `commands_rx` is closed, so let's stop the task.
+                return;
+            }
+        }
+    }
+}
+
+/// Function executed in the background for each TCP listener.
+async fn listener_task(
+    socket_id: u32,
+    listen_message_id: u64,
+    socket_addr: SocketAddr,
+    mut back_to_front: mpsc::Sender<BackToFront>,
+) {
+    // First step is to try create the listener.
+    let (listener, mut commands_rx) = match TcpListener::bind(socket_addr).await {
+        Ok(s) => {
+            let (tx, rx) = mpsc::channel(2);
+            let msg_to_front = BackToFront::ListenOk {
+                socket_id,
+                listen_message_id,
+                sender: tx,
+            };
+
+            if back_to_front.send(msg_to_front).await.is_err() {
+                return;
+            }
+
+            (s, rx)
+        }
+        Err(_) => {
+            let msg_to_front = BackToFront::ListenErr {
+                socket_id,
+                listen_message_id,
+            };
+            let _ = back_to_front.send(msg_to_front).await;
+            return;
+        }
+    };
+
+    // Now that we're connected and we have a `listener` and `commands_rx`, we can start reading
+    // and writing.
+    loop {
+        match commands_rx.next().await {
+            Some(FrontToBackListener::Accept { message_id }) => {
+                let (socket, _) = listener.accept().await.unwrap(); // TODO: don't unwrap
+                let msg_to_front = BackToFront::Accept { message_id, socket };
                 if back_to_front.send(msg_to_front).await.is_err() {
                     return;
                 }
