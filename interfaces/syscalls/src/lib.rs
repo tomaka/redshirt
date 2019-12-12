880749a29699d9700f667a67207e2d77e2c51bc7
Pierre Krieger <pierre.krieger1708@gmail.com>
2019-12-12 13:45:05 +0100

    Minor fixes

--- interfaces/syscalls/src/lib.rs
+++ interfaces/syscalls/src/lib.rs
@@ -42,10 +42,8 @@
 //! covered here), then it can receive interface messages using the [`next_interface_message`]
 //! function.
 //!
-//! The message can later be optionally be answered using the [`emit_answer`] function.
-//!
-//! Whether the sender of a message expects an answer can't be known, but should depend on the
-//! message itself.
+//! The message can later be optionally be answered using the [`emit_answer`] function. If the
+//! mesage is malformed, you can also use the [`emit_message_error`] function.
 //!
 //! There is no way for an interface handler to pro-actively send data to a process. Communication
 //! can only be done as a response to a message. This must be taken into account when designing
