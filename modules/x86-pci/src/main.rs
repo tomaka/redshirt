a282a65a2ded7f4c52c42349d503f96acdcac0e3
Pierre Krieger <pierre.krieger1708@gmail.com>
2019-12-13 21:02:21 +0100

    More work

--- modules/x86-pci/src/main.rs
+++ modules/x86-pci/src/main.rs
@@ -44,36 +44,54 @@ async fn async_main() {
     }
 }
 
+lazy_static::lazy_static! {
+    static ref PCI_DEVICES: hashbrown::HashMap<(u16, u16), (&'static str, &'static str)> = build_pci_info();
+}
+
 async unsafe fn read_pci_devices() {
-    let pci_devices = build_pci_info();
-
     // https://wiki.osdev.org/PCI
-    for bus_idx in 0 .. 4 {      // TODO: be smarter; check bus 0 then check for bridges
-        for device_idx in 0 .. 32 {
-            for func_idx in 0 .. 8 {    // TODO: check function 0 only first
-                let (vendor_id, device_id) = {
-                    let vendor_device = pci_cfg_read_u32(bus_idx, device_idx, func_idx, 0).await;
-                    let vendor_id = u16::try_from(vendor_device & 0xffff).unwrap();
-                    let device_id = u16::try_from(vendor_device >> 16).unwrap();
-                    (vendor_id, device_id)
-                };
+    let pci_devices = build_pci_info();
+    read_bus_pci_devices(0).await;
+}
 
-                if vendor_id == 0xffff {
-                    continue;
-                }
+async unsafe fn read_bus_pci_devices(bus_idx: u8) {
+    for device_idx in 0 .. 32 {
+        for func_idx in 0 .. 8 {    // TODO: check function 0 only first
+            let (vendor_id, device_id) = {
+                let vendor_device = pci_cfg_read_u32(bus_idx, device_idx, func_idx, 0).await;
+                let vendor_id = u16::try_from(vendor_device & 0xffff).unwrap();
+                let device_id = u16::try_from(vendor_device >> 16).unwrap();
+                (vendor_id, device_id)
+            };
 
-                let (vendor_name, device_name) = match pci_devices.get(&(vendor_id, device_id)) {
-                    Some((v, d)) => (Cow::Borrowed(*v), Cow::Borrowed(*d)),
-                    None => (
-                        Cow::Owned(format!("Unknown <0x{:x}>", vendor_id)),
-                        Cow::Owned(format!("Unknown <0x{:x}>", device_id))
-                    ),
-                };
-
-                let class_code = pci_cfg_read_u32(bus_idx, device_idx, func_idx, 0x8).await;
-                let bar0 = pci_cfg_read_u32(bus_idx, device_idx, func_idx, 0x10).await;
-                nametbd_stdout_interface::stdout(format!("PCI device: {} - {}; class = {:x}; bar0 = {:x}\n", vendor_name, device_name, class_code, bar0));
+            if vendor_id == 0xffff {
+                continue;
             }
+
+            let (_bist, header_ty, latency, cache_line) = {
+                let val = pci_cfg_read_u32(bus_idx, device_idx, func_idx, 0xc).await;
+                let bytes = val.to_be_bytes();
+                (bytes[0], bytes[1], bytes[2], bytes[3])
+            };
+
+            let (vendor_name, device_name) = match PCI_DEVICES.get(&(vendor_id, device_id)) {
+                Some((v, d)) => (Cow::Borrowed(*v), Cow::Borrowed(*d)),
+                None => (
+                    Cow::Owned(format!("Unknown <0x{:x}>", vendor_id)),
+                    Cow::Owned(format!("Unknown <0x{:x}>", device_id))
+                ),
+            };
+
+            let class_code = pci_cfg_read_u32(bus_idx, device_idx, func_idx, 0x8).await;
+            let bar0 = pci_cfg_read_u32(bus_idx, device_idx, func_idx, 0x10).await;
+            let bar1 = pci_cfg_read_u32(bus_idx, device_idx, func_idx, 0x14).await;
+            let bar2 = pci_cfg_read_u32(bus_idx, device_idx, func_idx, 0x18).await;
+            let bar3 = pci_cfg_read_u32(bus_idx, device_idx, func_idx, 0x1c).await;
+            let bar4 = pci_cfg_read_u32(bus_idx, device_idx, func_idx, 0x20).await;
+            let bar5 = pci_cfg_read_u32(bus_idx, device_idx, func_idx, 0x24).await;
+            nametbd_stdout_interface::stdout(format!("PCI device: {} - {}; class = {:x}; header = {:x}; bar = 0x{:x}, 0x{:x}, 0x{:x}, 0x{:x}, 0x{:x}, 0x{:x}\n", vendor_name, device_name, class_code, header_ty, bar0, bar1, bar2, bar3, bar4, bar5));
+
+            // TODO: wrong; need to enumerate other PCI buses
         }
     }
 }
