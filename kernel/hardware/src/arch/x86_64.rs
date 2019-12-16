39317f0c21725392a31631b17c06667e4433e923
Pierre Krieger <pierre.krieger1708@gmail.com>
2019-12-16 17:38:13 +0100

    Initialize APIC

--- kernel/hardware/src/arch/x86_64.rs
+++ kernel/hardware/src/arch/x86_64.rs
@@ -14,26 +14,28 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 use core::convert::TryFrom as _;
+use x86_64::registers::model_specific::Msr;
 use x86_64::structures::port::{PortRead as _, PortWrite as _};
 
 mod interrupts;
 
 pub unsafe fn init() {
-    // Initialize the PIC.
+    // Remap and disable the PIC.
     //
-    // The PIC (Programmable Interrupt Controller) is the old chip for triggering interrupts from
-    // hardware towards the CPU.
+    // The PIC (Programmable Interrupt Controller) is the old chip responsible for triggering
+    // on the CPU interrupts coming from the hardware.
     //
-    // Because of poor design decisions, it can trigger interrupts 0 to 32 on the CPU, which are
-    // normally reserved for software-related concerns. For example, the timer will by default
-    // trigger interrupt 8, which is also the double fault exception handler.
+    // Because of poor design decisions, it will be default trigger interrupts 0 to 32 on the CPU,
+    // which are normally reserved for software-related concerns. For example, the timer will by
+    // default trigger interrupt 8, which is also the double fault exception handler.
     //
     // In order to solve this issue, one has to reconfigure the PIC in order to make it trigger
     // interrupts between 32 and 48 rather than 0 to 16.
     //
-    // Note that we disable the PIC below. Why would we need to configure it if we disable it
-    // anyway? The answer is: I don't know. If we don't configure the PIC, BOCHS seems to trigger
-    // interrupts anyway despite it being disabled.
+    // Note that this code disables the PIC altogether. Despite the PIC being disabled, it is
+    // still possible to receive spurious interrupts. Hence the remapping.
+    u8::write_to_port(0xa1, 0xff);
+    u8::write_to_port(0x21, 0xff);
     u8::write_to_port(0x20, 0x10 | 0x01);
     u8::write_to_port(0xa0, 0x10 | 0x01);
     u8::write_to_port(0x21, 0x20);
@@ -42,10 +44,23 @@ pub unsafe fn init() {
     u8::write_to_port(0xa1, 2);
     u8::write_to_port(0x21, 0x01);
     u8::write_to_port(0xa1, 0x01);
-
-    // Disable the PIC by masking all interrupt vectors.
     u8::write_to_port(0xa1, 0xff);
     u8::write_to_port(0x21, 0xff);
+
+    // Set up the APIC.
+    let apic_base_addr = {
+        const APIC_BASE_MSR: Msr = Msr::new(0x1b);
+        let base_addr = APIC_BASE_MSR.read() & !0xfff;
+        APIC_BASE_MSR.write(base_addr | 0x800);     // Enable the APIC.
+        base_addr
+    };
+
+    // Enable spurious interrupts.
+    {
+        let svr_addr = usize::try_from(apic_base_addr + 0xf0).unwrap() as *mut u32;
+        let val = svr_addr.read_volatile();
+        svr_addr.write_volatile(val | 0x100);       // Enable spurious interrupts.
+    }
 
     interrupts::init();
 }
