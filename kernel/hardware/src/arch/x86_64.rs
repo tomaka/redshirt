ebd7a88530ba7b83e3d566bfd0970dc1c6a37fa4
Pierre Krieger <pierre.krieger1708@gmail.com>
2019-12-16 17:40:37 +0100

    More comments fixes

--- kernel/hardware/src/arch/x86_64.rs
+++ kernel/hardware/src/arch/x86_64.rs
@@ -25,12 +25,12 @@ pub unsafe fn init() {
     // The PIC (Programmable Interrupt Controller) is the old chip responsible for triggering
     // on the CPU interrupts coming from the hardware.
     //
-    // Because of poor design decisions, it will by default trigger interrupts 0 to 32 on the CPU,
+    // Because of poor design decisions, it will by default trigger interrupts 0 to 15 on the CPU,
     // which are normally reserved for software-related concerns. For example, the timer will by
     // default trigger interrupt 8, which is also the double fault exception handler.
     //
     // In order to solve this issue, one has to reconfigure the PIC in order to make it trigger
-    // interrupts between 32 and 48 rather than 0 to 16.
+    // interrupts between 32 and 47 rather than 0 to 15.
     //
     // Note that this code disables the PIC altogether. Despite the PIC being disabled, it is
     // still possible to receive spurious interrupts. Hence the remapping.
