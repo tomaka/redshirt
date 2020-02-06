1fa27be9f2a3da5c3f28b1494412ace774400f91
Pierre Krieger <pierre.krieger1708@gmail.com>
2020-02-06 23:10:53 +0100

    More link fixes

--- core/src/system.rs
+++ core/src/system.rs
@@ -348,7 +348,8 @@ impl SystemBuilder {
         self
     }
 
-    /// Shortcut for calling [`with_main_program`] multiple times.
+    /// Shortcut for calling [`with_main_program`](SystemBuilder::with_main_program) multiple
+    /// times.
     pub fn with_main_programs(self, hashes: impl IntoIterator<Item = ModuleHash>) -> Self {
         for hash in hashes {
             self.main_programs.push(hash);
