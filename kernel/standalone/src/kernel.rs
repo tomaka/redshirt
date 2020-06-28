bff0b867109e09ce0297338556e2c1440ef25163
Pierre Krieger <pierre.krieger1708@gmail.com>
2020-06-28 16:57:52 +0200

    Rustfmt

--- kernel/standalone/src/kernel.rs
+++ kernel/standalone/src/kernel.rs
@@ -78,8 +78,8 @@ where
         // TODO: use a better system than cfgs
         #[cfg(target_arch = "x86_64")]
         {
-            system_builder = system_builder
-                .with_startup_process(build_wasm_module!("../../../modules/e1000"));
+            system_builder =
+                system_builder.with_startup_process(build_wasm_module!("../../../modules/e1000"));
         }
         #[cfg(any(target_arch = "arm", target_arch = "aarch64"))]
         {
