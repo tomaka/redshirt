e255c6f1b57da6572bf861f18233e1c35ce93295
Pierre Krieger <pierre.krieger1708@gmail.com>
2020-07-25 19:40:18 +0200

    Small adjustement to _start (#517)

--- kernel/standalone/src/arch/x86_64/boot.rs
+++ kernel/standalone/src/arch/x86_64/boot.rs
@@ -28,163 +28,164 @@
 //! needed to run freestanding 64bits Rust code (i.e. a stack, paging, long mode), and call the
 //! `after_boot` Rust function.
 
+/// Entry point of the kernel.
+///
+/// Do **not** call manually.
 #[naked]
 #[no_mangle]
-extern "C" fn _start() {
-    unsafe {
-        asm!(r#"
-        .code32
-            // Disabling interruptions as long as we are not ready to accept them.
-            // This is normally already done by the bootloader, but I costs nothing to do it here
-            // again just in case.
-            cli
-        
-            // Check that we have been loaded by a multiboot2 bootloader.
-            cmp $0x36d76289, %eax
-            jne 5f
-        
-            // Clear the BSS segment.
-            // While this is normally not required, we do it anyway "just in case".
-            mov $__bss_start, %edi
-            mov $__bss_end, %ecx
-            sub $__bss_start, %ecx
-            jb 5f
-            mov $0, %al
-            cld
-            rep stosb %al, (%edi)
-        
-            // Now that the BSS is clear, we can put the value of EBX there.
-            mov %ebx, ({multiboot_info_ptr})
-        
-            // Check that our CPU supports extended CPUID instructions.
-            mov $0x80000000, %eax
-            cpuid
-            cmp $0x80000001, %eax
-            jb 5f
-        
-            // Check that our CPU supports the features that we need.
-            mov $0x80000001, %eax
-            cpuid
-            test $(1 << 29), %edx     // Test for long mode.
-            jz 5f
-        
-            // Everything is good. CPU is compatible.
-        
-            // Fill the first PML4 entry to point to the PDPT.
-            movl ${pdpt}, %eax
-            or $(1 << 0), %eax      // Present bit. Indicates that the entry is valid.
-            or $(1 << 1), %eax      // Read/write bit. Indicates that the entry is writable.
-            movl %eax, {pml4}
-        
-            // Fill the PDPT entries to point to the PDs.
-            mov $0, %ecx
-        2:  mov %ecx, %eax
-            shl $12, %eax                   // EAX <- ECX * 4096
-            addl ${pds}, %eax                 // EAX <- address of `pds` + ECX * 4096
-            or $(1 << 0), %eax              // Present bit. Indicates that the entry is valid.
-            or $(1 << 1), %eax              // Read/write bit. Indicates that the entry is writable.
-            movl %eax, {pdpt}(, %ecx, 8)      // PDPT[ECX * 8] <- EAX
-            inc %ecx
-            cmp $32, %ecx
-            jne 2b
-        
-            // Fill the PD entries to point to 2MiB pages.
-            mov $0, %ecx
-        3:  mov %ecx, %eax
-            shr $12, %eax                   // EAX <- ECX >> 12
-            movl %eax, {pds}+4(, %ecx, 8)     // PDs[4 + ECX * 8] <- EAX
-            mov %ecx, %eax                  // EAX <- ECX
-            shl $21, %eax                   // EAX <- ECX << 21
-            or $(1 << 0), %eax              // Present bit. Indicates that the entry is valid.
-            or $(1 << 1), %eax              // Read/write bit. Indicates that the entry is writable.
-            or $(1 << 7), %eax              // Indicates a 2MiB page.
-            movl %eax, {pds}(, %ecx, 8)       // PDs[ECX * 8] <- EAX
-            inc %ecx
-            cmp $(32 * 512), %ecx
-            jne 3b
-        
-            // Set up the control registers.
-            mov %cr0, %eax
-            and $(~(1 << 2)), %eax          // Clear emulation bit.
-            and $(~(1 << 31)), %eax         // Clear paging bit.
-            movl %eax, %cr0
-        
-            movl ${pml4}, %eax
-            movl %eax, %cr3
-        
-            movl $0, %eax
-            or $(1 << 10), %eax             // Set SIMD floating point exceptions bit.
-            or $(1 << 9), %eax              // Set OSFXSR bit, which enables SIMD.
-            or $(1 << 5), %eax              // Set physical address extension (PAE) bit.
-            movl %eax, %cr4
-        
-            // Set long mode with the EFER bit.
-            movl $0xc0000080, %ecx
-            rdmsr
-            or $(1 << 8), %eax
-            wrmsr
+unsafe extern "C" fn _start() {
+    asm!(r#"
+    .code32
+        // Disabling interruptions as long as we are not ready to accept them.
+        // This is normally already done by the bootloader, but I costs nothing to do it here
+        // again just in case.
+        cli
+    
+        // Check that we have been loaded by a multiboot2 bootloader.
+        cmp $0x36d76289, %eax
+        jne 5f
+    
+        // Clear the BSS segment.
+        // While this is normally not required, we do it anyway "just in case".
+        mov $__bss_start, %edi
+        mov $__bss_end, %ecx
+        sub $__bss_start, %ecx
+        jb 5f
+        mov $0, %al
+        cld
+        rep stosb %al, (%edi)
+    
+        // Now that the BSS is clear, we can put the value of EBX there.
+        mov %ebx, ({multiboot_info_ptr})
+    
+        // Check that our CPU supports extended CPUID instructions.
+        mov $0x80000000, %eax
+        cpuid
+        cmp $0x80000001, %eax
+        jb 5f
+    
+        // Check that our CPU supports the features that we need.
+        mov $0x80000001, %eax
+        cpuid
+        test $(1 << 29), %edx     // Test for long mode.
+        jz 5f
+    
+        // Everything is good. CPU is compatible.
+    
+        // Fill the first PML4 entry to point to the PDPT.
+        movl ${pdpt}, %eax
+        or $(1 << 0), %eax      // Present bit. Indicates that the entry is valid.
+        or $(1 << 1), %eax      // Read/write bit. Indicates that the entry is writable.
+        movl %eax, {pml4}
+    
+        // Fill the PDPT entries to point to the PDs.
+        mov $0, %ecx
+    2:  mov %ecx, %eax
+        shl $12, %eax                   // EAX <- ECX * 4096
+        addl ${pds}, %eax                 // EAX <- address of `pds` + ECX * 4096
+        or $(1 << 0), %eax              // Present bit. Indicates that the entry is valid.
+        or $(1 << 1), %eax              // Read/write bit. Indicates that the entry is writable.
+        movl %eax, {pdpt}(, %ecx, 8)      // PDPT[ECX * 8] <- EAX
+        inc %ecx
+        cmp $32, %ecx
+        jne 2b
+    
+        // Fill the PD entries to point to 2MiB pages.
+        mov $0, %ecx
+    3:  mov %ecx, %eax
+        shr $12, %eax                   // EAX <- ECX >> 12
+        movl %eax, {pds}+4(, %ecx, 8)     // PDs[4 + ECX * 8] <- EAX
+        mov %ecx, %eax                  // EAX <- ECX
+        shl $21, %eax                   // EAX <- ECX << 21
+        or $(1 << 0), %eax              // Present bit. Indicates that the entry is valid.
+        or $(1 << 1), %eax              // Read/write bit. Indicates that the entry is writable.
+        or $(1 << 7), %eax              // Indicates a 2MiB page.
+        movl %eax, {pds}(, %ecx, 8)       // PDs[ECX * 8] <- EAX
+        inc %ecx
+        cmp $(32 * 512), %ecx
+        jne 3b
+    
+        // Set up the control registers.
+        mov %cr0, %eax
+        and $(~(1 << 2)), %eax          // Clear emulation bit.
+        and $(~(1 << 31)), %eax         // Clear paging bit.
+        movl %eax, %cr0
+    
+        movl ${pml4}, %eax
+        movl %eax, %cr3
+    
+        movl $0, %eax
+        or $(1 << 10), %eax             // Set SIMD floating point exceptions bit.
+        or $(1 << 9), %eax              // Set OSFXSR bit, which enables SIMD.
+        or $(1 << 5), %eax              // Set physical address extension (PAE) bit.
+        movl %eax, %cr4
+    
+        // Set long mode with the EFER bit.
+        movl $0xc0000080, %ecx
+        rdmsr
+        or $(1 << 8), %eax
+        wrmsr
 
-            // Set up the GDT. It will become active only after we do the `ljmp` below.
-            lgdtl {gdt_ptr}
-        
-            // Activate long mode, and jump to the new segment.
-            mov %cr0, %eax
-            or $(1 << 0), %eax              // Set protected mode bit.
-            or $(1 << 1), %eax              // Set co-processor bit.
-            or $(1 << 4), %eax              // Set co-processor extension bit.
-            or $(1 << 31), %eax             // Set paging bit.
-            // The official manual says that instruction right after long mode switch must be a
-            // branch. Tutorials typically don't do that and it might not be strictly necessary,
-            // but to be safe let's follow what the manual says.
-            movl %eax, %cr0
-        
-            ljmp $8, $4f
+        // Set up the GDT. It will become active only after we do the `ljmp` below.
+        lgdtl {gdt_ptr}
+    
+        // Activate long mode, and jump to the new segment.
+        mov %cr0, %eax
+        or $(1 << 0), %eax              // Set protected mode bit.
+        or $(1 << 1), %eax              // Set co-processor bit.
+        or $(1 << 4), %eax              // Set co-processor extension bit.
+        or $(1 << 31), %eax             // Set paging bit.
+        // The official manual says that instruction right after long mode switch must be a
+        // branch. Tutorials typically don't do that and it might not be strictly necessary,
+        // but to be safe let's follow what the manual says.
+        movl %eax, %cr0
+    
+        ljmp $8, $4f
 
-        .code64
-        4:
-            // Set up the stack.
-            movq ${stack} + {stack_size}, %rsp
-        
-            movw $0, %ax
-            movw %ax, %ds
-            movw %ax, %es
-            movw %ax, %fs
-            movw %ax, %gs
-            movw %ax, %ss
+    .code64
+    4:
+        // Set up the stack.
+        movq ${stack} + {stack_size}, %rsp
+    
+        movw $0, %ax
+        movw %ax, %ds
+        movw %ax, %es
+        movw %ax, %fs
+        movw %ax, %gs
+        movw %ax, %ss
 
-            // Jump to our Rust code
-            // Pass as parameter the content of `multiboot_info_ptr`
-            mov ({multiboot_info_ptr}), %rdi
-            call {after_boot}
-            cli
-            hlt
+        // Jump to our Rust code
+        // Pass as parameter the content of `multiboot_info_ptr`
+        mov ({multiboot_info_ptr}), %rdi
+        call {after_boot}
+        cli
+        hlt
 
-        .code32
-        // Called if an unrecoverable error happens, such as an incompatible CPU.
-        5:
-            movb $'E', 0xb8000
-            movb $0xf, 0xb8001
-            movb $'r', 0xb8002
-            movb $0xf, 0xb8003
-            movb $'r', 0xb8004
-            movb $0xf, 0xb8005
-            movb $'o', 0xb8006
-            movb $0xf, 0xb8007
-            movb $'r', 0xb8008
-            movb $0xf, 0xb8009
-            cli
-            hlt
-        "#,
-            after_boot = sym super::after_boot,
-            gdt_ptr = sym super::gdt::GDT_POINTER,
-            multiboot_info_ptr = sym MULTIBOOT_INFO_PTR,
-            stack = sym MAIN_PROCESSOR_STACK,
-            stack_size = const MAIN_PROCESSOR_STACK_SIZE,
-            pml4 = sym PML4,
-            pdpt = sym PDPT,
-            pds = sym PDS,
-            options(noreturn, att_syntax)); // TODO: convert to Intel syntax
-    }
+    .code32
+    // Called if an unrecoverable error happens, such as an incompatible CPU.
+    5:
+        movb $'E', 0xb8000
+        movb $0xf, 0xb8001
+        movb $'r', 0xb8002
+        movb $0xf, 0xb8003
+        movb $'r', 0xb8004
+        movb $0xf, 0xb8005
+        movb $'o', 0xb8006
+        movb $0xf, 0xb8007
+        movb $'r', 0xb8008
+        movb $0xf, 0xb8009
+        cli
+        hlt
+    "#,
+        after_boot = sym super::after_boot,
+        gdt_ptr = sym super::gdt::GDT_POINTER,
+        multiboot_info_ptr = sym MULTIBOOT_INFO_PTR,
+        stack = sym MAIN_PROCESSOR_STACK,
+        stack_size = const MAIN_PROCESSOR_STACK_SIZE,
+        pml4 = sym PML4,
+        pdpt = sym PDPT,
+        pds = sym PDS,
+        options(noreturn, att_syntax)); // TODO: convert to Intel syntax
 }
 
 static mut MULTIBOOT_INFO_PTR: u64 = 0;
