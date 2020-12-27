bc6fb175f0da97781c120435d0d24da1e17a5e36
Pierre Krieger <pierre.krieger1708@gmail.com>
2020-12-27 20:35:56 +0100

    Fix doc and remove ffi module from kernel-debug

--- interfaces/kernel-debug/src/lib.rs
+++ interfaces/kernel-debug/src/lib.rs
@@ -13,24 +13,39 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-//! Loading WASM modules.
+//! Gathering kernel metrics.
 //!
-//! This interface is a bit special, as it is used by the kernel in order to load WASM modules.
+//! This interface provides a way to gather information from the kernel. In particular, the kernel
+//! returns [Prometheus](https://prometheus.io) metrics.
+//!
+//! # Message format
+//!
+//! - Sender sends a message with an empty body.
+//! - Handler sends back a Prometheus-compatible UTF-8 message.
+//!
+//! See [this page](https://prometheus.io/docs/instrumenting/exposition_formats/#text-format-details)
+//! for more information about the format.
+//!
 
 #![no_std]
 
 extern crate alloc;
 
 use alloc::{string::String, vec::Vec};
+use redshirt_syscalls::InterfaceHash;
 
-pub mod ffi;
+// TODO: this has been randomly generated; instead should be a hash or something
+pub const INTERFACE: InterfaceHash = InterfaceHash::from_raw_hash([
+    0x8c, 0xb4, 0xc6, 0xee, 0xc9, 0x29, 0xc5, 0xce, 0xaf, 0x46, 0x28, 0x74, 0x9e, 0x96, 0x72, 0x58,
+    0xea, 0xd2, 0xa2, 0xa2, 0xd6, 0xeb, 0x7d, 0xc8, 0xe3, 0x95, 0x0c, 0x0e, 0x53, 0xde, 0x8c, 0xba,
+]);
 
 /// Loads metrics from the kernel, as a Prometheus-compatible UTF-8 string.
-pub async fn get_metrics() -> String {
+pub async fn get_prometheus_metrics() -> String {
     unsafe {
         let response: redshirt_syscalls::EncodedMessage =
             redshirt_syscalls::emit_message_with_response(
-                &ffi::INTERFACE,
+                &INTERFACE,
                 redshirt_syscalls::EncodedMessage(Vec::new()),
             )
             .unwrap()
