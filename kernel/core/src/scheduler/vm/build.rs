239caab7674130c47f5d0a4016f98d8df72da69e
Pierre Krieger <pierre.krieger1708@gmail.com>
2022-01-16 11:56:33 +0100

    WIP

--- kernel/core/src/scheduler/vm/build.rs
+++ kernel/core/src/scheduler/vm/build.rs
@@ -14,8 +14,10 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 // TODO: main reference about API usage is https://github.com/bytecodealliance/wasmtime/blob/main/crates/environ/src/module_environ.rs
+// TODO: also see https://github.com/bytecodealliance/wasmtime/blob/main/crates/cranelift/src/compiler.rs
 
-use cranelift_codegen::*;
+use alloc::vec::Vec;
+use core::str::FromStr as _;
 
 pub fn build(wasm_bytecode: &[u8]) -> Result<(), wasmparser::BinaryReaderError> {
     for event in wasmparser::Parser::new(0).parse_all(wasm_bytecode) {
@@ -46,4 +48,17 @@ pub fn build(wasm_bytecode: &[u8]) -> Result<(), wasmparser::BinaryReaderError> 
     }
 
     Ok(())
+}
+
+fn test() {
+    let isa = {
+        // TODO: should detect the host features when building the isa, like in https://github.com/bytecodealliance/wasmtime/blob/main/cranelift/native/src/lib.rs
+        let builder = cranelift_codegen::isa::lookup(target_lexicon::triple!("x86_64")).unwrap(); // TODO: don't unwrap
+        let flags = cranelift_codegen::settings::Flags::new(cranelift_codegen::settings::builder());
+        builder.finish(flags)
+    };
+
+    let mut context = cranelift_codegen::Context::new();
+    let mut code_out = Vec::new();
+    context.compile_and_emit(&*isa, &mut code_out).unwrap(); // TODO: don't unwrap
 }
