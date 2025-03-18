02ae4e75903e94bdb7ac00ff1bb4a048b8511c4e
Pierre Krieger <pierre.krieger1708@gmail.com>
2025-03-18 11:26:24 +0100

    Add rerun-if-changed in build script

--- kernel-standalone-builder/build.rs
+++ kernel-standalone-builder/build.rs
@@ -14,6 +14,7 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 fn main() {
+    println!("cargo::rerun-if-changed=simpleboot");
     cc::Build::new()
         .file("simpleboot/wrapper.c")
         .compile("simpleboot");
